1a61d2b072f49d505da2ef3eb359f45d172b6c82
Kevin Irungu <kirungu481@gmail.com>
2024-11-04 13:34:31 +0300

    Updated application.rb

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module Whitespacebackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.assets.enabled = true
   end
 end
